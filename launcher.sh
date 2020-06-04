94a51ba0a853768c3e0b5e2aa05a42d8942caca6
prodriguezdefino <prodriguezdefino@gmail.com>
2020-06-04 00:21:13 +0000

    added creation of temp dir to the launcher script.

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p temps 
+
 GOOGLE_APPLICATION_CREDENTIALS=<SOME_GOOGLE_CREDENTIALS_FILE> java --enable-preview -jar target/bq-export-propagator-0.0.1-SNAPSHOT-bundled.jar \
   --project=<GCP_PROJECT> \
   --dataset=<BQ_DATASET_FOR_TEMP_TABLE> \
