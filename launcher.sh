ec861d5e79d014cffba979c5e5650f7a7c57353e
prodriguezdefino <prodriguezdefino@gmail.com>
2020-06-23 17:02:10 -0700

    removing preview features flag

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 
 mkdir -p temps 
 
-GOOGLE_APPLICATION_CREDENTIALS=<SOME_GOOGLE_CREDENTIALS_FILE> java --enable-preview -jar target/bq-export-propagator-0.0.1-SNAPSHOT-bundled.jar \
+GOOGLE_APPLICATION_CREDENTIALS=<SOME_GOOGLE_CREDENTIALS_FILE> java -jar target/bq-export-propagator-0.0.1-SNAPSHOT-bundled.jar \
   --project=<GCP_PROJECT> \
   --dataset=<BQ_DATASET_FOR_TEMP_TABLE> \
   --tableprefix=<BQ_TEMP_TABLE_PREFIX> \
